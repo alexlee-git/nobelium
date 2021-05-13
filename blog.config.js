6ce2a87a1aa23f1802641ad8d8821cb97b76d769
Alex Lee <alexlee.work@protonmail.com>
2021-05-13 18:39:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -40,7 +40,7 @@ const BLOG = {
     gitalkConfig: {
       repo: 'https://github.com/alexlee-git/gtalk', // The repository of store comments
       owner: 'alexlee-git',
-      admin: [alexlee-git],
+      admin: ['alexlee-git'],
       clientID: '2ff7861d07e256d38393',
       clientSecret: 'a55c574d4c4c46e47a6060086761d1a4d440747e',
       distractionFreeMode: false
